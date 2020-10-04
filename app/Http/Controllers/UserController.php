35aac6911e197ed8d378e879c1f1944538c70daa
vinicio <vinicio@landsagenciaweb.com.br>
2020-10-04 09:26:18 -0300

    controllers
    criação de controllers do programa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DomainController extends Controller
+class UserController.php extends Controller
 {
     /**
      * Display a listing of the resource.

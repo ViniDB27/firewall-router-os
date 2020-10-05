e7738d5f26d3da18e5f5349c23973b5b568cfec3
vinicio <viniciosdb@gmail.com>
2020-10-05 08:14:43 -0300

    routes error

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LocationController extends Controller
+class RegistrationController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class LocationController extends Controller
      */
     public function index()
     {
-        return view('locations');
+        //
     }
 
     /**
